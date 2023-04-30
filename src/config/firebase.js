45ab5847492451979b0adb6c2d375b9b005b4a68
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-30 15:08:46 +0100

    adding create, update, delete and upload files using react and firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider} from "firebase/auth"
 import { getFirestore } from 'firebase/firestore'
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA9trkR-lExfAVLP7KRyB-U3FbAHeoITGI",
@@ -17,3 +18,4 @@ export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 
 export const db = getFirestore(app);
+export const storage = getStorage(app);
